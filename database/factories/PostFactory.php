e7a31ef7da1cc1a8d8c3e7a81988600ba65cfb11
FM <Fastmoverr@gmail.com>
2023-06-23 09:11:25 -0500

    Added post mode, controller, seeder & factory, updated seeder and factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $user = \App\Models\User::first();
+        $title = fake()->sentence();
+        $slug = Str::of($title)->slug('-');
+
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'content' => fake()->paragraph(3),
+            'published_at' => fake()->dateTime(),
+            'author_id' => $user->id,
+        ];
+    }
+}
